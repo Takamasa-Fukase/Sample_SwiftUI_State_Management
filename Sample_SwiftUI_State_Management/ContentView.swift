1042b8eee3aeb144cc5582074c8e87c800b7ab22
Takamasa-Fukase <ultrafukase@gmail.com>
2024-11-28 18:12:47 +0900

    Initial Commit

---
+++ Sample_SwiftUI_State_Management/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sample_SwiftUI_State_Management
+//
+//  Created by ウルトラ深瀬 on 28/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sample_SwiftUI_State_Management/Sample_SwiftUI_State_ManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sample_SwiftUI_State_ManagementApp.swift
+//  Sample_SwiftUI_State_Management
+//
+//  Created by ウルトラ深瀬 on 28/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Sample_SwiftUI_State_ManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
